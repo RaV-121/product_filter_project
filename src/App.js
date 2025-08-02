ce3cee85ab7e42c2ae6d6731f6064e4c79cf7912
rafal <rafalbaczek8@gmail.com>
2025-08-02 22:43:27 +0200

    Basic version of site, added product component and catalog with pictures

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Product from './components/Product.js';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wybierz urządzenie
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <div className='MainContent'>
+        <input type="text" className="Search" placeholder='Search...'/>
+        <div className="Filters">
+          <div className='Filter'>
+            <p>Sortuj po:</p>
+            <select>
+              <option>Populartość</option>
+              <option>Cena</option>
+              <option>Pojemność</option>
+            </select>
+          </div>
+
+          <div className='Filter'>
+            <p>Funkcje:</p>
+            <select>
+              <option>Wszystkie</option>
+              <option>Drzwi AddWash</option>
+              <option>Panel AI Control</option>
+              <option>Silnik inwerterowy</option>
+              <option>Wyświetlacz elektroniczny</option>
+            </select>
+          </div>
+
+          <div className='Filter'>
+            <p>Klasa Energetyczna:</p>
+            <select>
+              <option>Wszystkie</option>
+              <option>A</option>
+              <option>B</option>
+              <option>D</option>
+            </select>
+          </div>
+
+          <div className='Filter'>
+            <p>Pojemność:</p>
+            <select>
+              <option>Wszystkie</option>
+              <option>9kg</option>
+              <option>8kg</option>
+              <option>10.5kg</option>
+            </select>
+          </div>
+        </div>
+        <div className="Products">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
