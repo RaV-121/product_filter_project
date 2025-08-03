e0de7b0a280667188986e2f465bc4741777fbdbd
rafal <rafalbaczek8@gmail.com>
2025-08-03 23:42:27 +0200

    Reorganization of file structure, added filters and more IU improvements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Product from './components/Product.js';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [filters, setFilters] = useState({
+    energyClass: "",
+    capacity: "",
+    selectedFeature: "",
+  });
+
+  const handleSort = (e) => {
+  const value = e.target.value;
+
+  const sorted = [...data];
+
+  if (value === "priceToHigher") {
+    sorted.sort((a, b) => a.cena - b.cena);
+  } else if (value === "priceToLower") {
+    sorted.sort((a, b) => b.cena - a.cena);
+  } else if (value === "capacityToHigher") {
+    sorted.sort((a, b) => a.pojemnosc.wartosc - b.pojemnosc.wartosc);
+  } else if (value === "capacityToLower") {
+    sorted.sort((a, b) => b.pojemnosc.wartosc - a.pojemnosc.wartosc);
+  } else if(value === "popular") {
+    sorted.sort((a, b) => b.popularnosc - a.popularnosc);
+  }
+  setData(sorted);
+  };
+
+
+  const handleFilterChange = e => {
+    const { name, value } = e.target;
+    console.log("name: ",name);
+    console.log("value: ",value);
+    setFilters(prev => ({ ...prev, [name]: value }));
+  };
+
+  const filteredData = data.filter(device => {
+    const matchEnergy = filters.energyClass ? device.klasa === filters.energyClass : true;
+    const matchCapacity = filters.capacity ? (device.pojemnosc.wartosc+'') === filters.capacity : true;
+    const matchFeature = filters.selectedFeature
+      ? device.funkcje.includes(filters.selectedFeature)
+      : true;
+
+    console.log("Energy ",matchEnergy)
+    return matchEnergy && matchCapacity && matchFeature;
+  });
+
+  useEffect(() => {
+    fetch('../data.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Błąd podczas pobierania pliku');
+        }
+        return response.json();
+      })
+      .then((jsonData) => {
+        console.log('Dane z Json: ', jsonData);
+        setData(jsonData);
+        console.log("dane zapisane", data);
+      })
+      .catch((error) => {
+        console.error('Wystąpił błąd:', error);
+      });
+}, []);
+
   return (
     <React.Fragment>
       <header className="App-header">
@@ -16,49 +79,50 @@ function App() {
         <div className="Filters">
           <div className='Filter'>
             <p>Sortuj po:</p>
-            <select>
-              <option>Populartość</option>
-              <option>Cena</option>
-              <option>Pojemność</option>
+            <select onChange={handleSort}>
+              <option value="popular">Populartość</option>
+              <option value="priceToHigher">Cena Rosnąco</option>
+              <option value="priceToLower">Cena Malejąco</option>
+              <option value="capacityToHigher">Pojemność Rosnąco</option>
+              <option value="capacityToLower">Pojemność Malejąco</option>
             </select>
           </div>
 
           <div className='Filter'>
             <p>Funkcje:</p>
-            <select>
-              <option>Wszystkie</option>
-              <option>Drzwi AddWash</option>
-              <option>Panel AI Control</option>
-              <option>Silnik inwerterowy</option>
-              <option>Wyświetlacz elektroniczny</option>
+            <select name="selectedFeature" value={filters.selectedFeature} onChange={handleFilterChange}>
+              <option value="">Wszystkie</option>
+              <option value="">Drzwi AddWash</option>
+              <option value="">Panel AI Control</option>
+              <option value="">Silnik inwerterowy</option>
+              <option value="">Wyświetlacz elektroniczny</option>
             </select>
           </div>
 
           <div className='Filter'>
             <p>Klasa Energetyczna:</p>
-            <select>
-              <option>Wszystkie</option>
-              <option>A</option>
-              <option>B</option>
-              <option>D</option>
+            <select name="energyClass" value={filters.energyClass} onChange={handleFilterChange}>
+              <option value="">Wszystkie</option>
+              <option value="A">A</option>
+              <option value="B">B</option>
+              <option value="D">D</option>
             </select>
           </div>
 
           <div className='Filter'>
             <p>Pojemność:</p>
-            <select>
-              <option>Wszystkie</option>
-              <option>9kg</option>
-              <option>8kg</option>
-              <option>10.5kg</option>
+            <select name="capacity" value={filters.capacity} onChange={handleFilterChange}>
+              <option value="">Wszystkie</option>
+              <option value="9">9kg</option>
+              <option value="8">8kg</option>
+              <option value="10.5">10.5kg</option>
             </select>
           </div>
         </div>
         <div className="Products">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          {filteredData.map((item, index) => (
+          <Product key={index} item={item}/>
+          ))}
         </div>
       </div>
     </React.Fragment>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
-function Product() {
+function Product({item}) {
+
+      function lastNumberFromValue(value) {
+      const str = value.toString();
+      if (str.includes('.')) {
+        return str.split('.')[1];
+      } else {
+        return '00';
+      }
+    }
+
   return (
     <div className="Product">
-        Product
+        <img src={"../img/"+item.picture+".png"} />
+        <div className="Details Opis">{item.opis}</div>
+
+        <div className="Details">
+          <div className="detailCategory">Pojemność (kg): </div>
+          <div className="detail">{item.pojemnosc.wartosc} {item.pojemnosc.jednostka}</div>
+        </div>
+
+        <div className="Details">
+          <div className="detailCategory">Wymiary (GxSxW): </div>
+          <div className="detail">{item.wymiary[0]} x {item.wymiary[1]} x {item.wymiary[2]} cm</div>
+        </div>
+
+        <div className="Details">
+          <div className="detailCategory ">Funkcje: </div>
+          <div className="detail">{item.funkcje.join(', ')}</div>
+        </div>
+
+        <div className="Details">
+          <div className="detailCategory">Klasa energetyczna </div>
+          <div className="detail energyClass">{item.klasa}</div>
+        </div>
+
+        <div className="Details">
+          <div className="detailCategory">Cena obowiązuje: </div>
+          <div className="detail Expiration">{item.cenaWaznosc.join(' - ')}</div>
+        </div>
+
+        <div className="Details">
+          <div className="price">
+            <span class="amount">{Math.trunc(item.cena)}</span>
+            <span class="fraction">{lastNumberFromValue(item.cena)}</span>
+            <span class="currency">zł</span>
+          </div>
+        </div>
+
+        <div className="Details">
+          <div className="Installmetns">{(item.cena/60).toFixed(2)} zł x 60 rat</div>
+        </div>
+
+        <button className="chooseButton">WYBIERZ</button>
+
     </div>
   )
 }
